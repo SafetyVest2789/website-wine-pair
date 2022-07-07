7e16db7b4132ea6c221e4387c8d51b152cec42ab
Safety Vest 2789 <jrnlife@gmail.com>
2022-07-07 01:57:38 -0400

    middleware added

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ MongoClient.connect(dbConnectionStr)
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
+app.set('view engine', 'ejs')
 app.use(express.static("index.html"))
 
 
