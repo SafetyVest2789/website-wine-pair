01e7d6bb1583e1cf27fe5caf83486116d00793ef
Safety Vest 2789 <jrnlife@gmail.com>
2022-07-07 02:23:22 -0400

    middleware added

---
+++ public/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#name').autocomplete({
+        source: async function(request,response) {
+            let data= await fetch(`http://localhost:8000/search?query=${request.term}`)
+                    .then(results => results.json())
+                    .then(results => results.map(result => {
+                        return {
+                            label: result.name,
+                            value: result.name,
+                            id: result._id
+                        }
+                    }))
+                response(data)
+                //console.log(response)
+        },
+        minLength: 2,
+        select: function(event, ui) {
+            console.log(ui.item.id)
+            fetch(`http://localhost:8000/get/${ui.item.id}`)
+                .then(result => result.json())
+                .then(result => {
+                    $('#food').empty()
+                    result.food.forEach(food =>
+                        {
+                            $("#food").append(`<li>${food}</li>`)
+                        })
+                        $('.bottom img').attr('src',result.image)
+                })
+        }
+    })
+})
